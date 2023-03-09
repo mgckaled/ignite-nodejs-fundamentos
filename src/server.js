c54b1258dc2e3d90d732657e7eba4cd2c2dc880d
mgckaled <marcelgckaled@gmail.com>
2023-03-09 13:01:53 -0300

    chore: configure eslint and prettier

--- src/server.js
+++ src/server.js
@@ -3,27 +3,27 @@ import http from 'node:http'
 const users = []
 
 const server = http.createServer((req, res) => {
-	const { method, url } = req
+  const { method, url } = req
 
-	console.log(req.headers)
+  console.log(req.headers)
 
-	if (method === 'GET' && url === '/users') {
-		return res
-			.setHeader('Content-type', 'application/json')
-			.end(JSON.stringify(users))
-	}
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
 
-	if (method === 'POST' && url === '/users') {
-		users.push({
-			id: 1,
-			name: 'John Doe',
-			email: 'johndoe@example.com'
-		})
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
 
-		return res.writeHead(201).end()
-	}
+    return res.writeHead(201).end()
+  }
 
-	return res.writeHead(404).end()
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
