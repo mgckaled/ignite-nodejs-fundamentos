881580b2a71301c1024f466b1c09875791777a5b
mgckaled <marcelgckaled@gmail.com>
2023-03-11 15:40:21 -0300

    feat: add request body as JSON

--- src/server.js
+++ src/server.js
@@ -2,10 +2,20 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  console.log(req.headers)
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if (method === 'GET' && url === '/users') {
     return res
@@ -14,15 +24,22 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com',
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
   }
 
+  if (req.body === null) {
+    return res.writeHead(400).end('Missing body with email and name.')
+  }
+
+  console.log(req.body)
   return res.writeHead(404).end()
 })
 
